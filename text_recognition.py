c55278ae4c4f102c965095669d617bae23e94359
Alejandro Ferrer-Peasley <36723243+alexferrer17@users.noreply.github.com>
2020-01-23 17:37:34 -0800

    Update text_recognition.py

--- text_recognition.py
+++ text_recognition.py
@@ -152,7 +152,7 @@ for (startX, startY, endX, endY) in boxes:
 	# wish to use the LSTM neural net model for OCR, and finally
 	# (3) an OEM value, in this case, 7 which implies that we are
 	# treating the ROI as a single line of text
-	config = ("-l eng --oem 1 --psm 7")
+	config = ("-l eng --oem 3 --psm 3")
 	text = pytesseract.image_to_string(roi, config=config)
 
 	# add the bounding box coordinates and OCR'd text to the list
