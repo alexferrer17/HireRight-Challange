06f417cfc56ba4472d38cb4fe5640acbcbf7830b
nicholasmirchandani <nmirchandani@chapman.edu>
2020-01-23 17:19:01 -0800

    Adds Regex For Label Detection instead of faking it

--- BasicFramework.py
+++ BasicFramework.py
@@ -1,3 +1,5 @@
+import re
+
 #Assumes (StartX, StartY, EndX, EndY) format of boxes
 def calcCenter(box):
     return ((box[2]+box[0])/2, (box[3]+box[1])/2)
@@ -5,6 +7,35 @@ def calcCenter(box):
 #Will be replaced with OCR
 def parseText(box):
     return "Sample Text for Box at " + str(box)
+
+def checkAllLabels(comparison_str):
+    if(patternMatch("Case (#|No|Num*)", comparison_str)):    #Matches Case #, Case No, Case Num, Case Number, etc.
+        return "Case Number"
+    elif(patternMatch("((Defendant|Party)(Name)?)", comparison_str)): #Matches Defendant Name, Party Name, Defendant, and Party
+        return "Name"
+    elif(patternMatch("(Sex|Gender)", comparison_str)):
+        return "Gender"
+    elif(patternMatch("(Year|Date) of Birth", comparison_str)):
+        return "Date of Birth"
+    elif(patternMatch("County", comparison_str)):
+        return "County"
+    elif(patternMatch("(Offense|Crime) Date", comparison_str)):
+        return "Offense Date"
+    elif(patternMatch("Arrest Date", comparison_str)):
+        return "Arrest Date"
+    elif(patternMatch("Fil[a-z]* Date", comparison_str)):
+        return "Filing Date"
+    elif(patternMatch("Disposition Date",comparison_str)):
+        return "Disposition Date"
+    elif(patternMatch("Status", comparison_str)):
+        return "Status"
+    return None
+
+def patternMatch(pattern_str, comparison_str):
+    pattern = re.compile(pattern_str)
+    if(pattern.match(comparison_str) == None):
+        return False
+    return True
 
 #Assumes there is more than one box on the screen
 def closestBox(box, boxes, boxCenters):
@@ -39,8 +70,12 @@ for i in range(0, len(boxes)):
     print("Box " + str(i) + " Center: " + str(calcCenter(boxes[i])))
     print("Box " + str(i) + " Text: " + str(parseText(boxes[i])))
 
-labelBoxes.append(boxes[0])
-boxLabels[labelBoxes[0]] = "Label 1"
+for i in boxText.keys():
+    name = checkAllLabels(boxText[i])
+    if(name != None):
+        labelBoxes.append(i)
+        boxLabels[i] = name
+
 #Loop through all of the labelBoxes and find the closestBox to them
 for i in labelBoxes:
     closestToLabel[i] = closestBox(i, boxes, boxCenters)
@@ -48,3 +83,5 @@ for i in labelBoxes:
 #Can do whatever we want with the text/box/center of the box we've found at this point to add it to our dataset
 for i in labelBoxes:
     print(boxLabels[i] + ": " + boxText[closestToLabel[i]])
+
+print(patternMatch("Fil[a-z]* Date", "Filing Date"))
